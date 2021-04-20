a9eccc74b2a37639f2cc07aea50eceb81a71b29c
DESKTOP-PNQ3FLQ\jahhy <jah.hyun12345@gmail.com>
2021-04-20 13:09:15 +0900

    * load more btn 설정 => CurrentPage state 값 + 1 되도록 설정 * fetchMovies() 함수 호출하여 달라지는 endpoint 값에 따라 state 값 저장할 수 있도록 설정 * setMovies() : 기존 Movies state에 새 값 더해지도록 설정

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -6,7 +6,7 @@ function GridCards(props) {
         <Col lg={6} md={8} xs={24}>
             <div style={{position:'relative'}}>
                 <a href={`/movie/${props.movieId}`}>
-                    <img style={{border:'1px solid black', width:'100%', height:'320px'}} src={props.movieImage} alt={props.movieName} />
+                    <img style={{width:'100%', height:'320px'}} src={props.movieImage} alt={props.movieName} />
                 </a>
             </div>
         </Col>
