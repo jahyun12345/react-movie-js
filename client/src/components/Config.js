f294007c2fe3f2a12b3976254145f0c2cb7cb4c0
DESKTOP-PNQ3FLQ\jahhy <jah.hyun12345@gmail.com>
2021-04-20 10:16:55 +0900

    * moviedb 관련 api 상수 처리(Config.js) => 공통되어 사용되는 api 링크 import하여 사용할 수 있도록

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+// moviedb api 중 상수부분 export
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+// 영화 이미지 api 중 상수부분 export
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
